12341276adc684c20354c9066ce3c8cca4d3a3b9
Dennis Cheng <m8939605@gmail.com>
2020-07-09 16:43:31 +0800

    Add address-3.c

---
+++ CH1/address-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int x = 100, y = 200, z = 300;
+    printf("x's address is %p\n", &x);
+    printf("y's address is %p\n", &y);
+    printf("z's address is %p\n\n", &z);
+
+    getchar();
+    return 0;
+}
